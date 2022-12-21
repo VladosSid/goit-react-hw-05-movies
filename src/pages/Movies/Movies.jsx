e52da6bf8d17a664c433fd2b50f0150727d5ce76
VladosSid <sidorsv121097@gmail.com>
2022-12-21 21:02:09 +0200

    Update Movies.jsx

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'; // eslint-disable-line
 
 export function Movies() {
   const [queryValue, setQueryValue] = useState('');
